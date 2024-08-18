7a74f4a45147d6062d78c87a09feb107e036b57b
mateopolci <mateopolci@hotmail.com>
2024-08-18 20:04:54 -0300

    exercises

--- main.go
+++ main.go
@@ -3,12 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    var twoD [3][5]int
-    for i := 0; i < 3; i++ {
-        for j := 0; j < 5; j++ {
-            twoD[i][j] = (i + 1) * (j + 1)
-        }
-        fmt.Println("Row", i, twoD[i])
-    }
-    fmt.Println("\nAll at once:", twoD)
+    months := []string{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
+    quarter2 := months[3:6]
+    fmt.Println("Slice original:", quarter2, len(quarter2), cap(quarter2))
+    quarter2extended := quarter2[:4]
+    fmt.Println("Extendido:", quarter2extended, len(quarter2extended), cap(quarter2extended))
 }
