413ca045bc2c7b24b5f1bf11299aa8cca0ddca34
mateopolci <mateopolci@hotmail.com>
2024-08-31 08:33:52 -0300

    .

--- dds/tp1/ejercicio1/ejercicio1.go
+++ dds/tp1/ejercicio1/ejercicio1.go
@@ -2,48 +2,109 @@ package main
 
 import (
 	"fmt"
-)
+	"bufio"
+	"os"
+	"strconv"
+	)
 
 func main(){
-
-
+	var option int
+	for {
+		fmt.Println("----------------------------")
+		fmt.Println("1: Calculator")
+		fmt.Println("2: Caesar Cipher")
+		fmt.Println("----------------------------")
+		fmt.Print("Select an option: ")
+		fmt.Scanf("%d\n", &option)
+		switch option {
+		case 1:
+			math()
+		case 2:
+			cipher()
+		}
+	}
 }
 
-func math(){
-	var option int
-	fmt.Println("Welcome to the math module, enter a number:")
-	
+func math() {
+	var (option, num1, num2, res int)
+	fmt.Println("----------------------------")
 	fmt.Println("1: Adding")
 	fmt.Println("2: Subtracting")
 	fmt.Println("3: Multiplying")
 	fmt.Println("4: Dividing")
+	fmt.Println("----------------------------")
+	fmt.Print("Select an option: ")
+	fmt.Scanf("%d\n", &option)
+	fmt.Print("Enter the first number: ")
+	fmt.Scanf("%d\n", &num1)
+	fmt.Print("Enter the second number: ")
+	fmt.Scanf("%d\n", &num2)
 	
 	switch option {
 	case 1:
-		fmt.Println("Add function")
+		fmt.Print("The sum is: ")
+		res=num1+num2
 	case 2:
-		fmt.Println("Subtract function")
+		fmt.Print("The difference is: ")
+		res=num1-num2
 	case 3:
-		fmt.Println("Multiply function")
+		fmt.Print("The product is: ")
+		res=num1*num2
 	case 4:
-		fmt.Println("Divide function")
+		fmt.Print("The quotient is: ")
+		res=num1/num2
 	}
+	fmt.Println(res)
 }
 
 func cipher(){
-	fmt.Println("Welcome to the Caesar Cipher module, enter a number:")
+	var option int
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	fmt.Println("Welcome to the Caesar Cipher module:")
+	fmt.Println("----------------------------")
 	fmt.Println("1: Cipher")
 	fmt.Println("2: Decipher")
+	fmt.Println("----------------------------")
+	fmt.Print("Select an option: ")
+	fmt.Scanf("%d", &option)
+	
+	var (
+		key, phrase int
+		alphabet[]rune
+		shiftedAlphabet[]rune
+	)
 
-	var key int
-	var option int
-
+	//Función para crear el alfabeto original
+	for i := 'a'; i <= 'z' ; i++ {
+		alphabet = append(alphabet, i)
+	}
+	
 	switch option {
-	case 1:
-		fmt.Println("Cipher function selected, enter a key value to cipher")
-		fmt.Scan(key)
-	case 2:
-		fmt.Println("Decipher function, enter a key value to decipher")
-		fmt.Scan(key)
+		case 1:
+			fmt.Printf("Enter a key value [0-26] to cipher: ")
+			fmt.Scan(&key)
+
+				//Crea el alfabeto desplazado en función de la llave
+				for i := 'a'; i <= 'z' ; i++ {
+					shiftedLetter := alphabet[(int(i)-'a'+key)%26]
+					shiftedAlphabet = append(shiftedAlphabet, shiftedLetter)
+				}
+
+			//Mostrar el alfabeto original
+			fmt.Println("Original Alphabet: ", string(alphabet))
+			//Mostrar el alfabeto Desplazado
+			fmt.Println("Shifted Alphabet: ", string(shiftedAlphabet))
+
+			fmt.Printf("Enter the phrase to cipher: ")
+			scanner.Scan()
+			phrase := scanner.Text()
+			fmt.Println(phrase)
+		case 2:
+			fmt.Println("Decipher function selected")
+			fmt.Print("Enter a key value [0-26] to decipher: ")
+			fmt.Scanf("%d", &key)
+			fmt.Print("Enter the phrase to cipher: ")
+			fmt.Scanf("%s", &phrase)
 	}
 }
