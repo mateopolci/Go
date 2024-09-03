f87dff8c7123916758756d72df8e65fc9c1a270f
mateopolci <mateopolci@hotmail.com>
2024-09-03 01:08:50 -0300

    generador

--- dds/tp1/ej2/frontend/src/main.js
+++ dds/tp1/ej2/frontend/src/main.js
@@ -1,2 +1,30 @@
 import './style.css';
 import './app.css';
+
+//Credentials generator
+function generateUsername() {
+    let username = Math.random().toString(36).slice(2);
+    return username;
+}
+function generatePassword() {
+    let password = Math.random().toString(36).slice(2);
+    return password;
+}
+
+//Buttons and div getters
+const generator = document.getElementById("generator");
+const credentials = document.getElementById("credentials");
+const cleaner = document.getElementById("cleaner");
+
+generator.addEventListener("click", () => {
+    credentials.innerHTML = `
+    <div class="border">
+        <p> Generated username: ${generateUsername()} </p>
+        <p> Generated password : ${generatePassword()} </p>
+    </div>
+    `;
+});
+
+cleaner.addEventListener("click", () => {
+    credentials.innerHTML = ""
+})
